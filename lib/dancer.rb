0dbf09f83cd7eb6a5be79148592bb01503659545
Michael Fuget <michaelfuget@gmail.com>
2020-12-05 00:59:59 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -4,7 +4,13 @@ require_relative './fancy_dance.rb'
 
 class Dancer
   
+  include Dance
+ 
   attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+  end
 
   extend MetaDancing
   extend FancyDance::ClassMethods
